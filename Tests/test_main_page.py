4fbaa1857e7df15cd50166cf8d3b03c937a4648c
SkyMisha <you@example.com>
2022-12-28 13:11:37 +0200

    Page transitions

--- Tests/test_main_page.py
+++ Tests/test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
@@ -9,8 +10,47 @@ from selenium.webdriver.common.by import By
 #     page.go_to_login_page()
 
 
-def test_guest_should_see_login_link(browser):
+# def test_guest_is_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     assert page.is_login_url()
+
+
+# def test_guest_should_be_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
+#
+#
+# def test_should_be_login_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_form()
+#
+#
+# def test_should_be_register_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = LoginPage(browser, link)
+#     main_page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_register_form()
+
+
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     login_page = page.go_to_login_page()
+#     login_page.should_be_login_page()
+
+
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    # def is_login_url(self):
+    #     return True if 'login' in self.url else False
+
+    def should_be_login_url(self):
+        assert "login" in self.url
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "fail"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "fail"
+
